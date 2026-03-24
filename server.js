10664139a7bb4e34b25885cdcdc9303a4401e6fe
Jordan Neumann <jordanneumann777@gmail.com>
2026-03-23 20:51:40 -0500

    seperate laptop and phone views (jackbox-esc)

--- server.js
+++ server.js
@@ -141,4 +141,5 @@ function getLocalIP() {
 const PORT = 3000;
 server.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running at http://${getLocalIP()}:${PORT}`);
+  console.log(`Host Link is http://${getLocalIP()}:${PORT}?role=host`);
 });
